ba28a0966f3c6ce7bce6934a5d3420ed4f0c5b1c
Kaushik-hub77 <kaushiknagaraj77@gmail.com>
2024-03-17 18:36:34 +0530

    updated search

--- collection.js
+++ collection.js
@@ -16,6 +16,78 @@ closenav.addEventListener("click", function () {
     sidenav.style.left = "-50%"
 })
 
+
+var search= document.getElementById("collectioninput")
+var cont= document.getElementById("n1")
+var productcont1= document.querySelector('.newarrivals1')
+var innerp1= productcont1.querySelectorAll("div")
+var productcont2= document.querySelector('.newarrivals2')
+var innerp3= productcont2.querySelectorAll("div")  
+var productcont3= document.querySelector('.newarrivals3')
+var innerp5= productcont3.querySelectorAll("div")
+
+
+search.addEventListener("keyup",function(){
+    var enteredvalue= event.target.value.toUpperCase()
+
+    for(count=0;count<innerp1.length;count=count+1)
+    {
+        var innerp2= innerp1[count].querySelector("h1").textContent
+
+        if(innerp2.toUpperCase().indexOf(enteredvalue)<0)
+
+        {
+            innerp1[count].style.display="none"
+         
+        }
+        else{
+            innerp1[count].style.display="block"
+        }
+    }
+})
+
+
+search.addEventListener("keyup",function(){
+    var enteredvalue= event.target.value.toUpperCase()
+for(count=0;count<innerp3.length;count=count+1)
+{
+    var innerp4= innerp3[count].querySelector("h1").textContent
+
+    if(innerp4.toUpperCase().indexOf(enteredvalue)<0)
+
+    {
+        innerp3[count].style.display="none"
+     
+    }
+    else{
+        innerp3[count].style.display="block"
+    }
+}
+})
+
+
+search.addEventListener("keyup",function(){
+    var enteredvalue= event.target.value.toUpperCase()
+for(count=0;count<innerp5.length;count=count+1)
+{
+    var innerp6= innerp5[count].querySelector("h1").textContent
+
+    if(innerp6.toUpperCase().indexOf(enteredvalue)<0)
+
+    {
+        innerp5[count].style.display="none"
+     
+    }
+    else{
+        innerp5[count].style.display="block"
+    }
+}
+})
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
     var checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
